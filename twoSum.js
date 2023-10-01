a2ea4bc80417d5bea02b2a59c791b5cd66265880
programfive <kennedysuedavid24@gmail.com>
2023-10-01 02:17:27 -0400

    edit twoSum complete

--- twoSum.js
+++ twoSum.js
@@ -14,8 +14,11 @@ const twoSum = function(nums, target) {
   }
 };
 
-const content=twoSum([3,2,3],9)//[0,2]
-console.log(content)
+twoSum([2,7,11,15],9)//[0,1]
+twoSum([3,2,4],6)//[1,2]
+twoSum([3,3],6)//[0,1]
+
+
 /*
 Example 1:
 
